ea5ef696695c7bf0fb7b9f4f9990375337b6ec5e
foubei <fouzi.tahir@gmail.com>
2017-12-20 10:57:26 +0000

    Add some dev/prod configurations

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,22 @@
-module.exports = ({ env }) => ({
-  plugins: {
-    stylelint: env === 'production' ? false : {},
-    colorguard: env === 'production' ? false : {},
-    'postcss-placehold': env === 'production' ? false : {},
-    'postcss-font-magician': {},
-    'rucksack-css': {},
-    'postcss-utilities': {
-      centerMethod: 'flexbox',
+module.exports = ({ options }) => {
+  return {
+    plugins: {
+      stylelint: options.env === 'production' ? false : {},
+      colorguard: options.env === 'production' ? false : {},
+      'postcss-placehold': options.env === 'production' ? false : {},
+      'postcss-font-magician': {},
+      'rucksack-css': {},
+      'postcss-utilities': {
+        centerMethod: 'flexbox',
+      },
+      'postcss-hocus': {},
+      'postcss-browser-reporter': options.env === 'production' ? false : {},
+      'postcss-combine-duplicated-selectors': options.env === 'production' ? {} : false,
+      'css-mqpacker': options.env === 'production' ? {} : false,
+      'postcss-flexbugs-fixes': options.env === 'production' ? {} : false,
+      'postcss-unprefix': options.env === 'production' ? {} : false,
+      autoprefixer: options.env === 'production' ? {} : false,
+      cssnano: options.env === 'production' ? {} : false,
     },
-    'postcss-hocus': {},
-    'postcss-browser-reporter': env === 'production' ? false : {},
-    'postcss-combine-duplicated-selectors': env === 'production' ? {} : false,
-    'css-mqpacker': env === 'production' ? {} : false,
-    'postcss-flexbugs-fixes': env === 'production' ? {} : false,
-    'postcss-unprefix': env === 'production' ? {} : false,
-    cssnano: env === 'production' ? {
-      preset: [
-        'advanced',
-      ],
-    } : false,
-    autoprefixer: env === 'production' ? {} : false,
-  },
-});
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,41 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  context: path.resolve(__dirname),
-  entry: {
-    index: [
-      '@js/index.js',
-      '@scss/index.scss',
-    ],
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        include: [
-          path.resolve(__dirname, 'src/'),
-        ],
-        loader: 'eslint-loader',
-        options: {
-          cache: true,
+module.exports = (env) => {
+  return {
+    context: path.resolve(__dirname),
+    entry: {
+      index: [
+        '@js/index.js',
+        '@scss/index.scss',
+      ],
+    },
+    module: {
+      rules: [{
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          include: path.resolve(__dirname, 'src/'),
+          loader: 'eslint-loader',
+          options: {
+            cache: true,
+          },
         },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        include: [
-          path.resolve(__dirname, 'src/'),
-        ],
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        exclude: [
-          path.resolve(__dirname, 'node_modules/'),
-        ],
-        include: [
-          path.resolve(__dirname, 'src/'),
-        ],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          include: [
+            path.resolve(__dirname, 'src/'),
+          ],
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.scss$/,
+          exclude: path.resolve(__dirname, 'node_modules/'),
+          include: path.resolve(__dirname, 'src/'),
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
               loader: 'css-loader',
               options: {
                 importLoaders: 1,
@@ -51,36 +44,46 @@ module.exports = {
             },
             {
               loader: 'postcss-loader',
-            },
-          ],
-        }),
-      },
-    ],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity,
-    }),
-  ],
-  recordsPath: path.join(__dirname, 'dist/webpack.records.json'),
-  resolve: {
-    alias: {
-      '@js': 'js/',
-      '@scss': 'scss/',
+              options: {
+                config: {
+                  ctx: {
+                    env: env.NODE_ENV,
+                  },
+                },
+              },
+            }],
+          }),
+        },
+      ],
     },
-    extensions: [
-      '.js',
-      '.scss',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+      }),
+      new ExtractTextPlugin('[name].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        minChunks: Infinity,
+      }),
     ],
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src/'),
-    ],
-  },
+    recordsPath: path.join(__dirname, 'dist/webpack.records.json'),
+    resolve: {
+      alias: {
+        '@js': 'js/',
+        '@scss': 'scss/',
+      },
+      extensions: [
+        '.js',
+        '.scss',
+      ],
+      modules: [
+        'node_modules',
+        path.resolve(__dirname, 'src/'),
+      ],
+    },
+  };
 };
